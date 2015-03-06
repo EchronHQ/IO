03eccd028b9a251a7d287f1a2dcfcfb1afa24836
Stijn Duynslaeger <info@echron.eu>
2015-03-06 20:13:28 +0100

    Add Http client (in progress) Fix typo

--- src/Echron/IO/AbstractIOClient.php
+++ src/Echron/IO/AbstractIOClient.php
@@ -2,7 +2,9 @@
 declare(strict_types = 1);
 namespace Echron\IO;
 
-abstract class AbstractClient
+abstract class AbstractIOClient
 {
+    abstract public function pull(string $remote, string $local);
 
+    abstract public function push(string $local, string $remote);
 }
---
+++ src/Echron/IO/Client/Http.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types = 1);
+namespace Echron\IO\Client;
+
+use Echron\IO\AbstractIOClient;
+
+class Http extends AbstractIOClient
+{
+
+    public function pull(string $remote, string $local)
+    {
+        // TODO: Implement pull() method.
+    }
+
+    public function push(string $local, string $remote)
+    {
+        // TODO: Implement push() method.
+    }
+}
