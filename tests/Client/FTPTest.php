516d82da019c9c826a8b1800cfdc726bc355d126
Stijn Duynslaeger <stijn@echron.com>
2022-10-11 11:56:52 +0100

    Add FTP test

---
+++ tests/Client/FTPTest.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+use Echron\IO\Client\Base;
+use Echron\IO\Client\SFTP;
+
+require_once 'AbstractTest.php';
+
+class FTPTest extends AbstractTest
+{
+
+    protected function getClient(): Base
+    {
+        $host = 'sftp-test';
+        $port = 22;
+        $client = new SFTP($host, $port);
+        $client->loginWithPassword('demo', 'demo');
+
+        return $client;
+    }
+
+    protected function getRemoteTestFilePath(): string
+    {
+        return '/testfile_' . uniqid() . '.txt';
+    }
+
+    protected function getRemoteTestFileContent(): string
+    {
+        // TODO: Implement getRemoteTestFileContent() method.
+    }
+}
